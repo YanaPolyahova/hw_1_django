3ea10e4ed46a4199df6981b8f8adafa83dd52b73
Yana <107498414+YanaPolyahova@users.noreply.github.com>
2022-11-18 19:31:35 +0300

    add files home.html and requirements.txt

--- app/views.py
+++ app/views.py
@@ -26,7 +26,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
-    current_time = datetime.datetime.now().time()
+    current_time = datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
